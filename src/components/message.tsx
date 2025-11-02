154cd92e71e54314b28a3a783feab9894bb2a1d1
Hans Eckbert <endzeit2030666@gmail.com>
2025-11-02 19:44:27 +0000

    die option mit den 3 punkenten neben der nachricht zum löschen und den a

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -110,7 +110,7 @@ export default function Message({ message, onQuote, onEdit, onDelete, onReact, s
        <div className={cn("relative", isCurrentUser ? "order-1" : "")}>
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon" className="h-6 w-6 opacity-0 group-hover:opacity-100 transition-opacity">
+                <Button variant="ghost" size="icon" className="h-6 w-6 transition-opacity">
                     <MoreHorizontal className="w-4 h-4" />
                 </Button>
             </DropdownMenuTrigger>
