757f23f0055d08c4172695d7eb097207054a07c8
Hans Eckbert <endzeit2030666@gmail.com>
2025-11-03 00:59:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4301936593-e6da1",
+  "appId": "1:647580734273:web:efe2980301b3e6ba7007c0",
+  "apiKey": "AIzaSyBu-ExMa8cMMqIlXgtLEDw9mbkTqIpWqDY",
+  "authDomain": "studio-4301936593-e6da1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "647580734273"
+};
